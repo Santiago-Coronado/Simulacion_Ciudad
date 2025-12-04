d9c92ad17f567d82699189ec87b64677e9457450
Santiago-Coronado <A01785558@tec.mx>
2025-12-03 23:01:29 -0600

    3rd version of mesa stuff :p

--- Simulacion/Server/trafficBase/server.py
+++ Simulacion/Server/trafficBase/server.py
@@ -1,7 +1,7 @@
 from traffic_base.agent import *
 from traffic_base.model import CityModel
 
-from mesa.visualization import Slider, SolaraViz, make_space_component
+from mesa.visualization import Slider, SolaraViz, make_space_component, make_plot_component
 from mesa.visualization.components import AgentPortrayalStyle
 
 
@@ -38,29 +38,52 @@ def agent_portrayal(agent):
 
     return portrayal
 
-
-def post_process(ax):
+def post_process_space(ax):
     ax.set_aspect("equal")
+    ax.set_xticks([])
+    ax.set_yticks([])
 
+
+def post_process_lines(ax):
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.9))
+
+
+model = CityModel(N=10, spawn_frequency=10, seed=42)
+
+# Define the colors for the line plot
+COLORS = {
+    "Active Cars": "blue",
+    "Cars Spawned": "orange",
+    "Cars Reached Destination": "green",
+}
 
 model_params = {
-    "N": 50,
+    "N": Slider("Maximum Cars", 10, 1, 100),
+    "spawn_frequency": Slider("Spawn Every N Steps", 10, 1, 10),
     "seed": {
         "type": "InputText",
         "value": 42,
         "label": "Random Seed",
     },
+    
 }
 
-model = CityModel(model_params["N"])
-
+# Create the space component
 space_component = make_space_component(
-    agent_portrayal, draw_grid=False, post_process=post_process
+    agent_portrayal,
+    draw_grid=False,
+    post_process=post_process_space,
+)
+
+# Create the line plot component
+lineplot_component_1 = make_plot_component(
+    COLORS,
+    post_process=post_process_lines,
 )
 
 page = SolaraViz(
     model,
-    components=[space_component],
+    components=[space_component, lineplot_component_1],
     model_params=model_params,
-    name="Random Model",
+    name="Traffic Simulation",
 )
